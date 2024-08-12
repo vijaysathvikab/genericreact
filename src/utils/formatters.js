b49ea01fd86aeb49d8f3b7797d2b7ed925a055b1
Vijay Sathvika B <vijaysathvikab@gmail.com>
2024-08-12 12:00:00 +0530

    feat(utils): add currency formatter

---
+++ src/utils/formatters.js
@@ -0,0 +1,14 @@
+export const formatCurrency = (amount, currency = 'USD') => {
+  return new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: currency,
+  }).format(amount);
+};
+
+export const formatDate = (dateString) => {
+  return new Date(dateString).toLocaleDateString('en-US', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  });
+};
